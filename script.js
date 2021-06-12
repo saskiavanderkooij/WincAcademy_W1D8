1181cdc299137cdb6484bc70cdc2d5dead704dea
Saskia vd Kooij <84019788+saskiavanderkooij@users.noreply.github.com>
2021-06-12 17:23:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+//const kiesVerfkleur = function () {
+//    console.log("De kleur van de muur wordt: rood " );
+//}
+
+//kiesVerfkleur();
+
+//const kiesVerfkleur = function (verf) {
+//    console.log("De kleur van de muur wordt: " + kleur);
+//}
+
+//const kleur = "geel";
+//kiesVerfkleur(kleur);
+
+//const kiesVerfkleur = function (locatie, kleur) {
+//    console.log("De kleur van de " + locatie + "muur wordt: " + kleur );
+//}
+
+//const muur = "slaapkamer";
+//const slaapkamerKleur = "paars";
+//kiesVerfkleur(muur, slaapkamerKleur);
+
+const kiesVerfkleur = function (locatie, kleur) {
+    console.log("De muur die geverfd wordt is in de: " + locatie);
+    console.log("De kleur van de muur wordt: " + kleur);
+}
+
+const muur = "slaapkamer";
+const slaapkamerKleur = "paars";
+kiesVerfkleur(muur, slaapkamerKleur)
